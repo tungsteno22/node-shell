4b998eb44a68bd802d35344ebd55110923a0a8dd
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2018-09-18 11:44:00 -0500

    second update

--- bash.js
+++ bash.js
@@ -4,10 +4,14 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', data => {
   const cmd = data.toString().trim(); // remoce the newline
-  const fs = require('fs');
+  //const fs = require('fs');
+  var fs = require('fs');
+  var path = require('path');
 
   if (cmd === 'pwd') {
-    process.stdout.write(fs);
+    process.stdout.write(process.cwd());
+    process.stdout.write('\nprompt > ');
+    //process.stdout.write(path.basename());
   } else {
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
